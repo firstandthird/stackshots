e3cc61cc1216fc5772b402c641f7dc64bc80d5c6
Antonio Laguna <a.laguna@funcion13.com>
2013-06-29 18:26:45 +0100

    Version 0.1.0
    Added browsers by default
    Added option to list browsers with os and versions
    Added option to change destination folder
    Added more messages to console to make it more verbose
    Throw an error if browser specified is not valid
    Fixed a bug in which specifying a url with http won't result in an image
    being saved

--- README.md
+++ README.md
@@ -14,10 +14,12 @@ Options:
   -u, --username     The email you use to log in to Browserstack                                                            [required]
   -p, --password     Your account's password                                                                                [required]
   -w, --website      The website(s) from which you want to get screenshots. Comma separated list
-  -b, --browser      The browser(s) from which you want to get screenshots. Comma separated list                            [required]
+  -b, --browser      The browser(s) from which you want to get screenshots. Comma separated list                            [default: "IE_8,IE_9,Chrome,Firefox"]
   -o, --orientation  Orientation of the device (portrait|landscape).                                                        [default: "portrait"]
-  -s, --os           Operating System of the browser separating version with underscore (windows_xp). Comma separated list
+  -s, --os           Operating System of the browser separating version with underscore (windows_xp). Comma separated list  [default: "windows_7"]
   -d, --device       The device(s) from which you want to get screenshots. Comma separated list
+  -f, --folder       Folder in which screenshots will be stored                                                              [default: Current folder]
+  -l, --ls           Instead of getting images, it will output a list of browsers and OS available
   -h, --help         Shows help info
 ```
 
@@ -50,7 +52,13 @@ www.google.com
 
 ### `browser`
 
-Browser from which you want to get an screenshot. If you don't specify a version, it will suppose you wan't to get the latest one:
+Browser from which you want to get an screenshot. By default they are:
+
+* Internet Explorer 8 & Internet Explorer 9
+* Chrome Latest
+* Firefox Latest
+
+If you don't specify a version, it will suppose you wan't to get the latest one:
 
 ```
 --browser ie
@@ -98,7 +106,7 @@ Orientation is usefull for Mobile Devices, otherwise, it is not. However, the pr
 
 ### `os`
 
-This is the Operating System in which you can request Screenshots.
+This is the Operating System in which you can request Screenshots. By default is Windows
 
 ```
 --os "OS X"
@@ -182,6 +190,24 @@ The device from which you want to take a screenshot.
 | **Android Browser** | Samsung Galaxy S | 2.2|
 | **Android Browser** | HTC Wildfire | 2.2|
 | **Android Browser** | LG Optimus 3D | 2.2|
+
+### `folder`
+
+The folder in which you want to store the screenshots. By default is the folder in which you are invoking the process.
+
+```
+--folder /tmp
+```
+
+### `ls`
+
+This option will ignore the rest of arguments and will output the list of browsers, versions and OS available so you have an idea of what is available.
+
+```
+Browser: ie
+OS: windows
+Versions: 6.0, 7.0, 10.0 Desktop, 8.0, 9.0, 10.0
+```
 
 ##Development and Tests
 
--- bin/browserscreenshot.js
+++ bin/browserscreenshot.js
@@ -9,6 +9,7 @@ var fs = require('fs'),
 var orientations = ['portrait','landscape'];
 
 var argv = optimist
+<<<<<<< HEAD
   .usage('browserscreenshot '+version+'\nUsage: $0 [opts]')
   .demand(['u','p','b'])
   .alias('u','username')
@@ -34,6 +35,41 @@ var argv = optimist
     }
   })
   .argv;
+=======
+    .usage('browserscreenshot '+version+'\nUsage: $0 [opts]')
+    .demand(['u','p'])
+    .alias('u','username')
+    .describe('u', 'The email you use to log in to Browserstack')
+    .alias('p','password')
+    .describe('p', 'Your account\'s password')
+    .alias('w','website')
+    .describe('w', 'The website(s) from which you want to get screenshots. Comma separated list')
+    .alias('b','browser')
+    .default('b','IE_8,IE_9,Chrome,Firefox')
+    .describe('b', 'The browser(s) from which you want to get screenshots. Comma separated list')
+    .alias('o','orientation')
+    .describe('o', 'Orientation of the device (portrait|landscape).')
+    .default('o','portrait')
+    .alias('s','os')
+    .describe('s', 'Operating System of the browser separating version with underscore (windows_xp). Comma separated list')
+    .default('s', 'windows_7')
+    .alias('d','device')
+    .describe('d', 'The device(s) from which you want to get screenshots. Comma separated list')
+    .alias('f', 'folder')
+    .describe('f', 'Folder in which screenshots will be stored')
+    .default('f', process.cwd())
+    .alias('l','ls')
+    .describe('l','Instead of getting images, it will output a list of browsers and OS available')
+    .boolean('l')
+    .alias('h','help')
+    .describe('h', 'Shows help info')
+    .check(function(argv){
+      if (orientations.indexOf(argv.orientation) === -1){
+        throw new Error('Orientation has to bee one of this values ' + orientations.join('|'))
+      }
+    })
+    .argv;
+>>>>>>> Version 0.1.0
 
 if (argv.help) {
   return optimist.showHelp(function(help) {
@@ -66,30 +102,56 @@ if (os){
 
 var client = new browserscreenshot({
   email : argv.username,
-  password : argv.password
+  password : argv.password,
+  folder : argv.folder
 }, function(){
-  var browsers = client.guessBrowsers(candidateBrowsers,os,osVersions, devices);
-  var urls = argv.website ? argv.website.split(',') : null, functions = [];
-
-  if (urls){
-    urls.forEach(function(url){
-      functions.push(function(cb){
-        var request = {
-          url : url,
-          orientation : argv.orientation,
-          browsers : browsers
-        };
-        console.log('Requesting images for %s', url);
-        client.getImages(request,cb);
-      });
-    });
-
-    async.series(functions,function(){
-      console.log('All urls have been downloaded!');
-    });
+  if (argv.l){
+    for (var browser in client.browsers){
+      if (client.browsers.hasOwnProperty(browser)) {
+        var versions = [];
+        console.log('Browser: %s', browser);
+        console.log('OS: %s', client.browsers[browser].os.join(', '));
+        client.browsers[browser].list.forEach(function(version){
+          var browserVersion = version.browser_version || version.os_version;
+          if (versions.indexOf(browserVersion) === -1){
+            versions.push(browserVersion);
+          }
+        });
+        console.log('Versions: %s', versions.join(', '));
+        console.log('');
+      }
+    }
   }
   else {
-    console.log('Since you provided no websites, I asume that you want to tests browser matching');
-    console.log(browsers);
+    var browsers = client.guessBrowsers(candidateBrowsers,os,osVersions, devices);
+    var urls = argv.website ? argv.website.split(',') : null, functions = [];
+
+    if (!browsers){
+      throw new Error('Invalid browsers supplied');
+    }
+    else {
+      if (urls){
+        urls.forEach(function(url){
+          functions.push(function(cb){
+            var request = {
+              url : url,
+              orientation : argv.orientation,
+              browsers : browsers
+            };
+            console.log('Requesting images for %s', url);
+            client.getImages(request,cb);
+          });
+        });
+
+        async.series(functions,function(){
+          console.log('All urls have been downloaded into "%s"!', client.destinationFolder);
+        });
+      }
+      else {
+        console.log('Since you provided no websites, I asume that you want to tests browser matching');
+        console.log(browsers);
+      }
+    }
   }
+
 });
